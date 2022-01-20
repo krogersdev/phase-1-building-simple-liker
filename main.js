7da5c081279f3574bf5e94db8ec56faaa1dcf083
Kevin Rogers <kevinarogers1@gmail.com>
2022-01-20 18:27:49 -0500

    commit tree 1eb8cf3ea1ec373c25939b97fa5484fb48a7265b

--- main.js
+++ main.js
@@ -3,7 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// document.addEventListener('DOMContentLoaded', hideModal); 
 
+//Event Listener 
+ const hearts = document.querySelectorAll('.like-glyph');
+ hearts.forEach(heart => heart.addEventListener('click',function(e) {
+  heartClick(e)
+  mimicServerCall()
+    .then(res => res)
+    .catch((error) => catchError(error))
+ }));
+ 
+ // Successful Click Event Handlers 
+function heartClick(e) {
+  if(e.target.innerText === FULL_HEART) {
+      e.target.innerText = EMPTY_HEART;
+      e.target.classList.remove('activated-heart');
+  } else {
+      e.target.innerText = FULL_HEART;
+      e.target.className = 'activated-heart';
+  }
+};
+
+//Fail Click Event Handlers 
+function catchError (error) {
+  document.querySelector("#modal > h2").innerText = `${error}`;
+  document.getElementById('modal').classList.remove('hidden');
+  setTimeout(hideModal, 3000); 
+}
+
+//Hides Modal
+function hideModal () {
+  const modal = document.getElementById('modal');
+  modal.className = ("hidden");
+}
 
 
 
